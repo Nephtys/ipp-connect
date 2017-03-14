2e910c41812c6bb7c5e168db76e944475d8d812b
Stéfanie <stefanie.loiseleur@gmail.com>
2017-03-14 15:00:13 +0100

    création appointment

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { EventManager} from 'ng-jhipster';
+import { Appointment } from '../entities/appointment';
+import { RestaurantService } from '../entities/restaurant';
+import { PersonService } from '../entities/person';
+import { AppointmentService } from '../entities/appointment';
+
 
 import { Account, LoginModalService, Principal } from '../shared';
 
@@ -17,11 +22,19 @@ export class HomeComponent implements OnInit {
   lng = 2.28882;
     account: Account;
     modalRef: NgbModalRef;
+    public name;
+    public evtType;
+    public date;
+    public time;
+    public address;
 
     constructor(
         private principal: Principal,
         private loginModalService: LoginModalService,
-        private eventManager: EventManager
+        private eventManager: EventManager,
+        private restaurantService: RestaurantService,
+        private personService: PersonService,
+        private appointmentService: AppointmentService
     ) {
         }
 
@@ -46,5 +59,21 @@ export class HomeComponent implements OnInit {
 
     login() {
         this.modalRef = this.loginModalService.open();
+    }
+
+    onSubmit() {
+        let appointment = new Appointment();
+        appointment.title = this.name;
+        appointment.time = this.date;
+        this.restaurantService.find(1).subscribe(data1 => {
+            appointment.restaurant = data1;
+            this.personService.find(1).subscribe(data2 => {
+                appointment.person = data2;
+
+                console.log(appointment);
+                this.appointmentService.create(appointment).subscribe(data3 => console.log(data3), error => console.log(error));
+            });
+
+        });
     }
 }
